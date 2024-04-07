44a217efb4e2da36a37cc3845747024f25e44111
Fatemeh Haji Agha Bozorgi <86429575+FatemehHABozorgi@users.noreply.github.com>
2024-04-07 10:28:03 +0330

    Update ExtendLVM.sh

--- ExtendLVM.sh
+++ ExtendLVM.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-lsblk
+lsblk -f
 
 echo -e "\nEnter the name of the disk to use (e.g., /dev/sdX):"
 read disk_name
 
 # Check if the disk already belongs to the volume group
-if vgs "$disk_name" &>/dev/null; then
+if pvs -o vg_name "$disk_name" &>/dev/null; then
     echo "Error: The disk '$disk_name' is already part of a volume group."
     exit 1
 fi
@@ -29,11 +29,12 @@ if [[ -b "$disk_name" ]]; then
                 if xfs_growfs /dev/mapper/vgdata-lvdata; then
                     echo "File system grown successfully."
                     echo "#####################################################################"
-                    lsblk
+                    lsblk -f
                     echo "#####################################################################"
                     fdisk -l
                     echo "#####################################################################"
                     df -h
+                    echo "Don't forget to check /var/log/message for disk errors."
                     echo "Done :)"
                 else
                     echo "Error: Failed to grow file system."
