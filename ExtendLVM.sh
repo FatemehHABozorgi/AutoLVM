0503841d02893ab01c01e4a8c2e4762bb8cb40ff
Fatemeh Haji Agha Bozorgi <86429575+FatemehHABozorgi@users.noreply.github.com>
2024-04-07 12:26:18 +0330

    Update ExtendLVM.sh

--- ExtendLVM.sh
+++ ExtendLVM.sh
@@ -11,7 +11,7 @@ read -a disk_names
 echo -e "\nConfirm the disk list with yes or no:"
 read var
 
-if [[ "$var" == "y" ]]; then
+if [[ "$var" == "yes" ]]; then
 	echo "#####################################################################"
 	# Check if any of the disks are already part of the volume group
 	for disk_name in "${disk_names[@]}"; do
