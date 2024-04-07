3bb521a65cc2879125826b47598ea28be59ad1f5
Fatemeh Haji Agha Bozorgi <86429575+FatemehHABozorgi@users.noreply.github.com>
2024-04-07 10:50:07 +0330

    Update ExtendLVM.sh

--- ExtendLVM.sh
+++ ExtendLVM.sh
@@ -2,52 +2,70 @@
 
 lsblk -f
 
-echo -e "\nEnter the name of the disk to use (e.g., /dev/sdX):"
-read disk_name
+echo -e "\nEnter y or n:"
 
-# Check if the disk already belongs to the volume group
-if pvs -o vg_name "$disk_name" &>/dev/null; then
-    echo "Error: The disk '$disk_name' is already part of a volume group."
-    exit 1
-fi
+read var
 
-if [[ -b "$disk_name" ]]; then
-    echo "#####################################################################"
-    echo "Creating physical volume..."
-    if pvcreate "$disk_name"; then
-        echo "Physical volume created successfully."
-        echo "#####################################################################"
-        echo "Extending volume group..."
-        if vgextend vgdata "$disk_name"; then
-            echo "Volume group extended successfully."
-            echo "#####################################################################"
-            echo "Extending logical volume..."
-            if lvextend -l +95%FREE /dev/vgdata/lvdata; then
-                echo "Logical volume extended successfully."
-                echo "#####################################################################"
-                echo "Growing file system..."
-                if xfs_growfs /dev/mapper/vgdata-lvdata; then
-                    echo "File system grown successfully."
-                    echo "#####################################################################"
-                    lsblk -f
-                    echo "#####################################################################"
-                    fdisk -l
-                    echo "#####################################################################"
-                    df -h
-                    echo "Don't forget to check /var/log/message for disk errors."
-                    echo "Done :)"
-                else
-                    echo "Error: Failed to grow file system."
-                fi
-            else
-                echo "Error: Failed to extend logical volume."
-            fi
-        else
-            echo "Error: Failed to extend volume group."
-        fi
-    else
-        echo "Error: Failed to create physical volume."
-    fi
-else
-    echo "Error: The specified disk '$disk_name' does not exist or is not a block device."
+if [[ "$var" == "y" ]]; then
+	echo "#####################################################################"
+
+	echo -e "\nEnter the name(s) of the disk(s) to use (e.g., /dev/sdX /dev/sdY):"
+	read -a disk_names
+
+	# Check if any of the disks are already part of the volume group
+	for disk_name in "${disk_names[@]}"; do
+		if pvs -o vg_name "$disk_name" &>/dev/null; then
+			echo "Error: The disk '$disk_name' is already part of a volume group."
+			exit 1
+		fi
+	done
+
+	for disk_name in "${disk_names[@]}"; do
+		if [[ -b "$disk_name" ]]; then
+			echo "#####################################################################"
+			echo "Creating physical volume on $disk_name..."
+			if pvcreate "$disk_name"; then
+				echo "Physical volume created successfully."
+				echo "#####################################################################"
+				echo "Extending volume group with $disk_name..."
+				if vgextend vgdata "$disk_name"; then
+					echo "Volume group extended successfully with $disk_name."
+				else
+					echo "Error: Failed to extend volume group with $disk_name."
+					exit 1
+				fi
+			else
+				echo "Error: Failed to create physical volume on $disk_name."
+				exit 1
+			fi
+		else
+			echo "Error: The specified disk '$disk_name' does not exist or is not a block device."
+			exit 1
+		fi
+	done
+
+	echo "#####################################################################"
+	echo "Extending logical volume..."
+	if lvextend -l +95%FREE /dev/vgdata/lvdata; then
+		echo "Logical volume extended successfully."
+		echo "#####################################################################"
+		echo "Growing file system..."
+		if xfs_growfs /dev/mapper/vgdata-lvdata; then
+			echo "File system grown successfully."
+			echo "#####################################################################"
+			lsblk -f
+			echo "#####################################################################"
+			fdisk -l
+			echo "#####################################################################"
+			df -h
+			echo "#####################################################################"
+			
+			echo "Don't forget to check /var/log/message for disk errors."
+			echo "Done :)"
+		else
+			echo "Error: Failed to grow file system."
+		fi
+	else
+		echo "Error: Failed to extend logical volume."
+	fi
 fi
